fed83089ff25ba2ddcf25c8aa2c7fa268e9ed442
Md Abu Zafar <mdabuzafar78@gmail.com>
2019-08-17 15:39:39 +0530

    Initial commit to Setup project

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+// routes
+app.get('/', (req, res) =>
+  res.json({
+    msg: 'Welcome to the ContactKeeper API...',
+  }),
+);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
